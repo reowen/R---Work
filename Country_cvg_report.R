e0f70a57510ec73dbccbbea7d7bc405a8645f5a7
unknown <reowen@RTI-025761.RTI.NS>
2014-11-25 14:45:26 -0500

    Make disease-specific dataframes

--- Country_cvg_report.R
+++ Country_cvg_report.R
@@ -20,9 +20,14 @@ raw = master_file[, colnames]
 #   names(raw)[i] = colnames[i]
 # }
 
-for(i in 11:length(colnames)){
-  raw[, colnames[i]] = as.numeric(as.character(raw[, colnames[i]]))
-}
+l = length(colnames)
+raw[,(11:l)] = as.numeric(as.character(raw[,(11:l)]))
+
+# for(i in 11:length(colnames)){
+#   raw[, colnames[i]] = as.numeric(as.character(raw[, colnames[i]]))
+# }
+
+# raw[is.na(raw[,(11:l)]) == TRUE, (11:l)] = 0
 
 for(i in 11:length(colnames)){
   raw[is.na(raw[,colnames[i]]) == TRUE, colnames[i]] = 0
@@ -33,8 +38,8 @@ for(i in 11:length(colnames)){
 all_targeted = c("persons_targeted_all_funding", "persons_targeted_all_funding_r1", "persons_targeted_all_funding_r2")
 all_treated = c("persons_treated_all_funding", "persons_treated_all_funding_r1", "persons_treated_all_funding_r2")
 
-raw['number_targeted_all'] = apply(raw[, all_targeted], 1, max)
-raw['number_treated_all'] = apply(raw[, all_treated], 1, max)
+raw['targeted'] = apply(raw[, all_targeted], 1, max)
+raw['treated'] = apply(raw[, all_treated], 1, max)
 
 
 #create number targeted and treated (usaid support)
@@ -63,41 +68,74 @@ raw['sac_treated'] = apply(raw[, sac_treated], 1, max)
 #####################################################
 
 keep_cols = c("country_name", "region_name", "district_name", "fiscal_year", "disease", 
-              "persons_at_risk", "sac_at_risk", 'number_targeted_all', 'number_treated_all', 
+              "persons_at_risk", "sac_at_risk", 'targeted', 'treated', 
               'number_targeted_usaid', 'number_treated_usaid', 'sac_targeted', 'sac_treated')
 
 #Use only SAR2, and the above specified columns
 raw_cvg = raw[raw$reporting_period == '2nd SAR (October-September)', keep_cols]
 
 #Code program coverage variables
-raw_cvg['prg_cvg'] = round((raw_cvg[, 'number_treated_all'] / raw_cvg[, 'number_targeted_all']), digits=4)
+raw_cvg['prg_cvg'] = round((raw_cvg[, 'treated'] / raw_cvg[, 'targeted']), digits=4)
 raw_cvg['prg_cvg_usaid'] = round((raw_cvg[, 'number_treated_usaid'] / raw_cvg[, 'number_targeted_usaid']), digits=4)
 
 #Code epi coverage variables
-raw_cvg['epi_cvg'] = round((raw_cvg[,'number_treated_all'] / raw_cvg[,'persons_at_risk']), digits=4)
+raw_cvg['epi_cvg'] = round((raw_cvg[,'treated'] / raw_cvg[,'persons_at_risk']), digits=4)
 raw_cvg['epi_cvg_usaid'] = round((raw_cvg[,'number_treated_usaid'] / raw_cvg[,'persons_at_risk']), digits=4)
 
 raw_cvg['sac_epi_cvg'] = NA
 raw_cvg[(raw_cvg$disease == 'Schisto' | raw_cvg$disease == 'STH'), 'sac_epi_cvg'] = round((raw_cvg[(raw_cvg$disease == 'Schisto' | raw_cvg$disease == 'STH'), 'sac_treated'] / raw_cvg[(raw_cvg$disease == 'Schisto' | raw_cvg$disease == 'STH'), 'sac_at_risk']), digits=4)
 
 
-#Write to CSV
-write.csv(raw_cvg, "C:\\Users\\reowen\\Documents\\Datasets\\raw_cvg_trends.csv")
+# #Write to CSV
+# write.csv(raw_cvg, "C:\\Users\\reowen\\Documents\\Datasets\\raw_cvg_trends.csv")
 
 #RESHAPE, CONDENSE THE DATASET: fiscal years in columns
 
-keep_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year', 'disease', 
-              'persons_at_risk', 'sac_at_risk', 'number_targeted_all', 'number_treated_all', 
-              'prg_cvg', 'epi_cvg', 'sac_epi_cvg')
+reg_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year', 'disease', 
+              'persons_at_risk', 'targeted', 'treated', 'prg_cvg', 'epi_cvg')
 
-reshape_me = raw_cvg[, keep_cols]
-  
-w = reshape(reshape_me, 
-            timevar = 'fiscal_year', 
-            idvar = c("country_name", "region_name", "district_name", "disease"), 
-            direction = 'wide')
+lf = raw_cvg[raw_cvg[,'disease'] == 'LF', reg_cols]
+lf = lf[with(lf, order(fiscal_year)), ]
+lf = reshape(lf,
+             timevar = 'fiscal_year',
+             idvar = c("country_name", "region_name", "district_name", "disease"), 
+             direction = 'wide')
 
-write_data = w[with(w, order(disease, country_name, region_name, district_name)), ]
+oncho = raw_cvg[raw_cvg[,'disease'] == 'Oncho', reg_cols]
+oncho = oncho[with(oncho, order(fiscal_year)), ]
+oncho = reshape(oncho, 
+                timevar = 'fiscal_year', 
+                idvar = c("country_name", "region_name", "district_name", "disease"), 
+                direction = 'wide')
+
+trachoma = raw_cvg[raw_cvg[,'disease'] == 'Trachoma', reg_cols]
+trachoma = trachoma[with(trachoma, order(fiscal_year)), ]
+trachoma = reshape(trachoma, 
+                   timevar = 'fiscal_year', 
+                   idvar = c("country_name", "region_name", "district_name", "disease"), 
+                   direction = 'wide')
+
+
+sac_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year', 'disease', 
+             'persons_at_risk', 'sac_at_risk', 'targeted', 'treated', 
+             'prg_cvg', 'epi_cvg', 'sac_epi_cvg')
+
+sch = raw_cvg[raw_cvg[,'disease'] == 'Schisto', sac_cols]
+sch = sch[with(sch, order(fiscal_year)), ]
+sch = reshape(sch, 
+              timevar = 'fiscal_year', 
+              idvar = c("country_name", "region_name", "district_name", "disease"), 
+              direction = 'wide')
+
+sth = raw_cvg[raw_cvg[,'disease'] == 'STH', sac_cols]
+sth = sth[with(sth, order(fiscal_year)), ]
+sth = reshape(sth, 
+              timevar = 'fiscal_year', 
+              idvar = c("country_name", "region_name", "district_name", "disease"), 
+              direction = 'wide')
+
+
+
 
 write.csv(write_data, "C:\\Users\\reowen\\Documents\\Datasets\\cvg_trends_wide.csv")
 
