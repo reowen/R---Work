a99d97f6290b9ead478d9f2ccbdb0fbb4d38a07a
unknown <reowen@RTI-025761.RTI.NS>
2014-11-25 15:56:10 -0500

    Fix encoding error in country_cvg_report

--- Country_cvg_report.R
+++ Country_cvg_report.R
@@ -15,14 +15,16 @@ colnames = c("country_name", "region_name", "district_name", "district_global_ta
              "sac_treated_all_funding_r1", "sac_treated_all_funding_r2")
 
 raw = master_file[, colnames]
+rm(master_file)
 
-l = length(colnames)
-raw[,(11:l)] = as.numeric(as.character(raw[,(11:l)]))
+for(i in 11:length(colnames)){
+  raw[, colnames[i]] = as.numeric(as.character(raw[, colnames[i]]))
+}
 
 for(i in 11:length(colnames)){
   raw[is.na(raw[,colnames[i]]) == TRUE, colnames[i]] = 0
 }
-
+rm(colnames)
 #Create number targeted and treated (all support)
 
 all_targeted = c("persons_targeted_all_funding", "persons_targeted_all_funding_r1", "persons_targeted_all_funding_r2")
@@ -30,7 +32,7 @@ all_treated = c("persons_treated_all_funding", "persons_treated_all_funding_r1",
 
 raw['targeted'] = apply(raw[, all_targeted], 1, max)
 raw['treated'] = apply(raw[, all_treated], 1, max)
-
+rm(all_targeted, all_treated)
 
 #create number targeted and treated (usaid support)
 
@@ -39,6 +41,7 @@ usaid_treated = c("persons_treated_usaid_funding", "persons_treated_usaid_fundin
 
 raw['number_targeted_usaid'] = apply(raw[, usaid_targeted], 1, max)
 raw['number_treated_usaid'] = apply(raw[, usaid_treated], 1, max)
+rm(usaid_targeted, usaid_treated)
 
 #create SAC treated variable
 
@@ -51,7 +54,7 @@ sac_treated = c("sac_treated_all_funding", "sac_treated_usaid_funding",
 
 raw['sac_targeted'] = apply(raw[, sac_targeted], 1, max)
 raw['sac_treated'] = apply(raw[, sac_treated], 1, max)
-
+rm(sac_targeted, sac_treated)
 
 #####################################################
 ##Condense the dataset to something more manageable##
@@ -63,6 +66,7 @@ keep_cols = c("country_name", "region_name", "district_name", "fiscal_year", "di
 
 #Use only SAR2, and the above specified columns
 raw_cvg = raw[raw$reporting_period == '2nd SAR (October-September)', keep_cols]
+rm(raw, keep_cols)
 
 #Code program coverage variables
 raw_cvg['prg_cvg'] = round((raw_cvg[, 'treated'] / raw_cvg[, 'targeted']), digits=4)
@@ -83,32 +87,35 @@ raw_cvg[(raw_cvg$disease == 'Schisto' | raw_cvg$disease == 'STH'), 'sac_epi_cvg'
 #RESHAPE, CONDENSE THE DATASET by disease: fiscal years in columns#
 ###################################################################
 
-reg_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year', 'disease', 
+reg_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year',
               'persons_at_risk', 'targeted', 'treated', 'prg_cvg', 'epi_cvg')
 
 lf = raw_cvg[raw_cvg[,'disease'] == 'LF', reg_cols]
 lf = lf[with(lf, order(fiscal_year)), ]
 lf = reshape(lf,
              timevar = 'fiscal_year',
-             idvar = c("country_name", "region_name", "district_name", "disease"), 
+             idvar = c("country_name", "region_name", "district_name"), 
              direction = 'wide')
+lf = lf[with(lf, order(country_name, region_name, district_name)), ]
 
 oncho = raw_cvg[raw_cvg[,'disease'] == 'Oncho', reg_cols]
 oncho = oncho[with(oncho, order(fiscal_year)), ]
 oncho = reshape(oncho, 
                 timevar = 'fiscal_year', 
-                idvar = c("country_name", "region_name", "district_name", "disease"), 
+                idvar = c("country_name", "region_name", "district_name"), 
                 direction = 'wide')
+oncho = oncho[with(oncho, order(country_name, region_name, district_name)), ]
 
 trachoma = raw_cvg[raw_cvg[,'disease'] == 'Trachoma', reg_cols]
 trachoma = trachoma[with(trachoma, order(fiscal_year)), ]
 trachoma = reshape(trachoma, 
                    timevar = 'fiscal_year', 
-                   idvar = c("country_name", "region_name", "district_name", "disease"), 
+                   idvar = c("country_name", "region_name", "district_name"), 
                    direction = 'wide')
+trachoma = trachoma[with(trachoma, order(country_name, region_name, district_name)), ]
 
-
-sac_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year', 'disease', 
+rm(reg_cols)
+sac_cols = c('country_name', 'region_name', 'district_name', 'fiscal_year', 
              'persons_at_risk', 'sac_at_risk', 'targeted', 'treated', 
              'prg_cvg', 'epi_cvg', 'sac_epi_cvg')
 
@@ -116,17 +123,21 @@ sch = raw_cvg[raw_cvg[,'disease'] == 'Schisto', sac_cols]
 sch = sch[with(sch, order(fiscal_year)), ]
 sch = reshape(sch, 
               timevar = 'fiscal_year', 
-              idvar = c("country_name", "region_name", "district_name", "disease"), 
+              idvar = c("country_name", "region_name", "district_name"), 
               direction = 'wide')
+sch = sch[with(sch, order(country_name, region_name, district_name)), ]
 
 sth = raw_cvg[raw_cvg[,'disease'] == 'STH', sac_cols]
 sth = sth[with(sth, order(fiscal_year)), ]
 sth = reshape(sth, 
               timevar = 'fiscal_year', 
-              idvar = c("country_name", "region_name", "district_name", "disease"), 
+              idvar = c("country_name", "region_name", "district_name"), 
               direction = 'wide')
+sth = sth[with(sth, order(country_name, region_name, district_name)), ]
 
+rm(sac_cols)
 
+#save excel workbook, multi-tabs
 library(xlsx)
 cvg_trends = createWorkbook()
 lf_tab = createSheet(wb=cvg_trends, sheetName="LF")
