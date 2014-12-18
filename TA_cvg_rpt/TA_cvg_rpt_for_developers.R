71de5195c7186dae8178ed3c4decdaed98d62d85
Russell Owen <russ.e.owen@gmail.com>
2014-12-18 10:22:30 -0500

    Add TIS and TAS to TA_cvg_rpt_for_developers.R

--- TA_cvg_rpt/TA_cvg_rpt_for_developers.R
+++ TA_cvg_rpt/TA_cvg_rpt_for_developers.R
@@ -34,8 +34,10 @@ rm(colnames)
 
 #To calculated the treated and targeted columns:
 #Take the max of the columns specified in "all_targeted" and "all_treated" vectors
-all_targeted = c("persons_targeted_all_funding", "persons_targeted_all_funding_r1", "persons_targeted_all_funding_r2")
-all_treated = c("persons_treated_all_funding", "persons_treated_all_funding_r1", "persons_treated_all_funding_r2")
+all_targeted = c("persons_targeted_all_funding", "persons_targeted_all_funding_r1", "persons_targeted_all_funding_r2", 
+                 "persons_targeted_usaid_funding", "persons_targeted_usaid_funding_r1", "persons_targeted_usaid_funding_r2")
+all_treated = c("persons_treated_all_funding", "persons_treated_all_funding_r1", "persons_treated_all_funding_r2", 
+                "persons_treated_usaid_funding", "persons_treated_usaid_funding_r1", "persons_treated_usaid_funding_r2")
 
 #note these are the max values across persons targeted, persons targeted r1 and r2
 raw['targeted'] = apply(raw[, all_targeted], 1, max)
@@ -61,16 +63,61 @@ raw_cvg['prg_cvg'] = round((raw_cvg[, 'treated'] / raw_cvg[, 'targeted']), digit
 #Code epi coverage variable
 raw_cvg['epi_cvg'] = round((raw_cvg[,'treated'] / raw_cvg[,'persons_at_risk']), digits=4)
 
-
-###################################################################
-#RESHAPE, CONDENSE THE DATASET: fiscal years in columns#
-###################################################################
+##############################################
+###Pull out only relevant indicators to keep##
+##############################################
 
 reg_cols = c('country_name', 'region_name', 'district_name', 'disease', 'fiscal_year',
              "disease_distribution", 'persons_at_risk', 'targeted', 'treated', 
              'prg_cvg', 'epi_cvg', 'people_living_mda_acheived')
 
-final = raw_cvg[, reg_cols]
+cvg_final = raw_cvg[, reg_cols]
+
+
+###################################################################
+## Pull out TAS and TIS indicators ##
+###################################################################
+
+#me_master is a .csv version of the me.tab offline file
+me_master = read.csv("C:\\Users\\reowen\\Documents\\Offline Files\\me_master.csv")
+
+colnames = c('country_name', 'region_name', 'district_name', 'applicable_disease',
+             'fiscal_year', 'reporting_period',	'assessment_type', 'assessment_completed')
+
+dsa = me_master[me_master$reporting_period == '2nd SAR (October-September)', colnames]
+names(dsa)[4] = 'disease'
+dsa = dsa[dsa$disease != 'NULL', ]
+
+
+#These are valid values for TAS type
+TAS_values = c('LF TAS: Post-MDA Surveillance I', 'LF TAS: Stop MDA', 
+               'LF Transmission Assessment Survey (TAS)', 'LF TAS: Post-MDA Surveillance II')
+
+#TAS column shows the TAS type, only if the assessment has been completed
+dsa['TAS'] = dsa$assessment_type
+dsa[(!(dsa$assessment_type %in% TAS_values) | dsa$assessment_completed != 'yes'), 'TAS'] = NA
+
+#Valid values for a TIS... the 'health impact surveys' one i think is a data entry error
+TIS_values = c('Trachoma impact survey', 'Trachoma health impact surveys')
+
+dsa['TIS'] = dsa$assessment_type
+dsa[(!(dsa$assessment_type %in% TIS_values) | dsa$assessment_completed != 'yes'), 'TIS'] = NA
+
+
+#pull out only relevant indicators to keep
+keep = c('country_name', 'region_name', 'district_name', 'disease',
+         'fiscal_year', 'TAS', 'TIS')
+
+dsa_final = dsa[, keep]
+
+###################################################################
+#Merge DSA and MDA data -- RESHAPE, CONDENSE THE DATASET: fiscal years in columns#
+###################################################################
+
+# merge dsa_final and cvg_final
+final = merge(dsa_final, cvg_final, 
+              by=c('country_name', 'region_name', 'district_name', 'disease', 'fiscal_year'), 
+              all = TRUE)
 
 final = final[with(final, order(fiscal_year)), ]
 
@@ -78,5 +125,7 @@ final = reshape(final,
                 timevar = 'fiscal_year', 
                 idvar = c('country_name', 'region_name', 'district_name', 'disease'), 
                 direction = 'wide')
-  
+
+final = final[with(final, order(disease, country_name, region_name, district_name)), ]
+
 write.csv(final, "C:\\Users\\reowen\\Documents\\Datasets\\mda_cvg_report.csv")
